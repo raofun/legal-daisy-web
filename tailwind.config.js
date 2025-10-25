9aece1bf988667762ada8cd17c4e3589f90fd7c4
raofun_ <raofunazad851@gmail.com>
2025-10-25 19:09:43 +0600

    Header Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                'law-prmry': 'rgba(182, 140, 90, 1)',
+                'law-bg': 'rgba(182, 140, 90, 0.1)',
+                'dark-3': '#707070',
+
+            },
+        },
+    },
+    plugins: [],
 }
